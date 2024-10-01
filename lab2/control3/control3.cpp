64c6c87c8fbf376e3c89b08645437667da707d58
Igor <igorsmirnov604@gmail.com>
2024-10-01 16:14:05 +0300

    labs 1-4

---
+++ lab2/control3/control3.cpp
@@ -0,0 +1,37 @@
+﻿// control3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    system("chcp 1251");
+
+    int money;
+    std::cout << "Укажиите сумму: "; std::cin >> money;
+
+    int summ = 0;
+    int coins = 0;
+    while (summ != money) {
+        if (summ + 10 <= money) {
+            summ += 10;
+            coins++;
+        }
+        else if (summ + 5 <= money) {
+            summ += 5; 
+            coins++;
+        }
+        else if (summ + 2 <= money) {
+            summ += 2;
+            coins++;
+        }
+        else {
+            summ++;
+            coins++;
+        }
+    }
+
+    std::cout << "Использовано " << coins << " монет";
+    return 0;
+}
+
---
+++ lab2/upr2task2/task2upr2.cpp
@@ -0,0 +1,29 @@
+﻿// task2upr2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	system("chcp 1251");
+
+	int a, b, temp;
+
+	std::cout << "a = "; std::cin >> a;
+	std::cout << "b = "; std::cin >> b;
+
+	while (a != b) {
+		if (a > b) {
+			a -= b;
+		}
+		else { 
+			b -= a;
+		}
+	}
+
+	std::cout << "НОД = " << a << std::endl;
+
+	return 0;
+}
+
+
