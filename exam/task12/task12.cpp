fdf6bf6aac98659d789f71361eaf4abea3ce7d1e
Igor <igorsmirnov604@gmail.com>
2025-01-22 22:44:37 +0300

    fix last task

--- exam/task12/task12.cpp
+++ exam/task12/task12.cpp
@@ -8,16 +8,21 @@ int main()
     std::srand(static_cast<unsigned int>(std::time(0)));
 
     int ans = std::rand() % 100 + 1;
-
+    int cnt = 0;
     std::cout << "Мое число в диапазоне от 1 до 100." << std::endl;
     int var = 0;
     do {
+        cnt++;
         std::cout << "Введите число: ";
         std::cin >> var;
         if (var < ans) std::cout << "Мое число больше" << std::endl;
         if (var > ans) std::cout << "Мое число меньше" << std::endl;
     } while (var != ans);
     std::cout << "Верно! Мое число: " << ans << std::endl;
+    if (cnt < 3) std::cout << "мастер";
+    else if (cnt < 6) std::cout << "Отличный";
+    else if (cnt < 8) std::cout << "Хороший";
+    else std::cout << "Плохой";
     return 0;
 }
 
